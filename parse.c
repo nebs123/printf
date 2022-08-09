6142d015e4e228e4f3d36938d9c28023c91c9d3f
Nebiyou Yonas Metaferia <nebsdivo@gmail.com>
2022-08-09 05:19:50 -0700

    printf: primitive handling for when % specifier ends with \0

--- parse.c
+++ parse.c
@@ -29,7 +29,9 @@ int _printf(const char *format, ...)
 
 	if (format == NULL)
 		return (-1);
-
+	if (format[0] == '%' && format[1] == '\0')
+		return (-1);
+	
 	va_start(list, format);
 	while (*format)
 	{
