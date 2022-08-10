dc367c01d49092d084e19e504a26695108ec6e67
Nebiyou Yonas Metaferia <nebsdivo@gmail.com>
2022-08-09 23:44:10 -0700

    prinf: force commit

--- tests/test3.c
+++ tests/test3.c
@@ -13,7 +13,7 @@ int main(void)
 
 	for (i = 0; i < 100; i++)
 	{
-		num = num * 2;
+		num = i;
 		n1 = printf("%d and %i, b is %b\n", num, -num, num);
 		n2 = _printf("%d and %i, b is %b\n", num, -num, num);
 
@@ -24,9 +24,9 @@ int main(void)
 	(void) num;
 	(void) i;
 
-	n = printf("%d and %i %u %o %x %X\n", 123422342, -2342423234, 255, 255, 255,255);
+	n = printf("%d and %i %u %o %x %X\n", 100012, -100012, 255, 255, 255,255);
 	printf("n: %d\n", n);
-	n = _printf("%d and %i %u %o %x %X\n",123422342, -2342423234, 255, 255, 255, 255);
+	n = _printf("%d and %i %u %o %x %X\n",100012, -100012, 255, 255, 255, 255);
 	printf("n: %d\n", n);
 	return (0);
 }
